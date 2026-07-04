159c0fff547348699da7a4f4fe31c784eddaa113
katya356 <yekaterina.okruzhnova@bk.ru>
2026-07-04 22:14:47 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
+from ui_main import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("Конвертер старых мер")
+    app.setApplicationVersion("1.0")
+    app.setFont(QFont("Segoe UI", 10))
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
